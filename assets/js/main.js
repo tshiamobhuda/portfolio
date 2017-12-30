6f9bf4a2b5001b24052c37970f979c58b9b6f03e
Tshiamo Bhuda <tshiamobhuda@miway.co.za>
2017-12-30 11:53:38 +0200

    Added Bootstrap 3 Collapsable Navbar - Added HTML Shiv & Respond.js for IE8 Support - Added *Forgotten* html5 meta tags - Added hooks to stack/unstack navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,13 @@ $('a[href*=#]:not([href=#])').click(function () {
 
     return false;
 });
+
+//Stack menu when collapsed
+$('#bs-example-navbar-collapse-1').on('show.bs.collapse', function() {
+    $('.nav-pills').addClass('nav-stacked');
+});
+
+//Unstack menu when not collapsed
+$('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
+    $('.nav-pills').removeClass('nav-stacked');
+});
